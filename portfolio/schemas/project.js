4a2f9c3a03d5f448393368f9cbf840ff928b9b81
Ithihas Reddy <ithihasreddy@Ithihass-MacBook-Pro.local>
2021-03-04 00:57:04 +0530

    latest

---
+++ portfolio/schemas/project.js
@@ -0,0 +1,24 @@
+export default{
+    name: "project",
+    title: "Project",
+    type: "document",
+    fields :[
+        {
+            name: "title",
+            type: "string",
+        },
+        {
+            name: "preview",
+            type: "image",
+        },
+        {
+            name: "Link",
+            type:"url",
+
+        },
+        {
+            name: "description",
+            type: "text",
+        }
+    ]
+}
